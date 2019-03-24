df7adfbaf19b995e8238ce2cc7625ad320c0f727
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-24 12:42:41 +0000

    Reformatted code

--- static/js/drum.js
+++ static/js/drum.js
@@ -1,19 +1,20 @@
-window.addEventListener('keydown', function(e) {
+function playDrum(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
     if (!audio) return; //stop the function from running all together
     audio.currentTime = 0; //rewind to the start
     audio.play();
     key.classList.add('playing');
-
-
-});
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => {
-    key.addEventListener('transitionend', removeTransition);
-});
+}
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return; // skip the transition event if it's not transform
     this.classList.remove('playing');
 }
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => {
+    key.addEventListener('transitionend', removeTransition);
+});
+window.addEventListener('keydown', playDrum);
+
